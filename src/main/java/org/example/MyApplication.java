9450ba8f79464d2b5d27c3b61614aa3fd634f786
Johannes Passing <jpassing@hotmail.com>
2022-04-21 09:27:50 +0200

    Change listen IP, port based on environment

--- src/main/java/org/example/MyApplication.java
+++ src/main/java/org/example/MyApplication.java
@@ -2,15 +2,12 @@ package org.example;
 
 import io.undertow.Undertow;
 import io.undertow.servlet.Servlets;
-import io.undertow.servlet.api.DeploymentInfo;
 import org.jboss.resteasy.core.ResteasyDeploymentImpl;
 import org.jboss.resteasy.plugins.server.undertow.UndertowJaxrsServer;
-import org.jboss.resteasy.spi.ResteasyDeployment;
 import org.jboss.weld.environment.servlet.Listener;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import java.net.http.WebSocket;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,28 +15,39 @@ import java.util.Set;
 public class MyApplication extends Application {
   @Override
   public Set<Class<?>> getClasses() {
-    Set<Class<?>> classes = new HashSet<>();
-    classes.add(MyResource.class); // Resource implemented in the previous section
+    var classes = new HashSet<Class<?>>();
+    classes.add(MyResource.class);
     return classes;
   }
 
   public static void main(String[] args) {
-    UndertowJaxrsServer server = new UndertowJaxrsServer();
+    var server = new UndertowJaxrsServer();
 
-    ResteasyDeployment deployment = new ResteasyDeploymentImpl();
-    deployment.setApplicationClass(MyApplication.class.getName()); // Application implemented in the previous section
+    //
+    // Configure deployment.
+    //
+    var deployment = new ResteasyDeploymentImpl();
+    deployment.setApplicationClass(MyApplication.class.getName());
     deployment.setInjectorFactoryClass("org.jboss.resteasy.cdi.CdiInjectorFactory"); // set CDI injector factory
 
-    DeploymentInfo deploymentInfo = server.undertowDeployment(deployment, "/");
+    var deploymentInfo = server.undertowDeployment(deployment, "/");
     deploymentInfo.setClassLoader(Application.class.getClassLoader());
-    deploymentInfo.setDeploymentName("Minimal Undertow RESTeasy and Weld CDI Setup"); // set name of deployment
+    deploymentInfo.setDeploymentName("Default");
     deploymentInfo.setContextPath("/");
-    deploymentInfo.addListener(Servlets.listener(Listener.class)); // add Weld listener to deployment
-
+    deploymentInfo.addListener(Servlets.listener(Listener.class));
     server.deploy(deploymentInfo);
 
-    Undertow.Builder builder = Undertow.builder()
-        .addHttpListener(8080, "localhost"); // access the server on http://localhost:8080, note that "localhost" should be "0.0.0.0" if you wish for others in the network to connect.
+    //
+    // Start Undertow server.
+    //
+    var runningOnAppEngine = System.getenv().containsKey("GAE_SERVICE");
+    var listenPort = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+
+    var builder = Undertow
+        .builder()
+        .addHttpListener(
+            listenPort,
+            runningOnAppEngine ? "0.0.0.0" : "localhost");
     server.start(builder);
   }
 }
--- src/main/java/org/example/MyResource.java
+++ src/main/java/org/example/MyResource.java
@@ -1,20 +1,15 @@
 package org.example;
 
 import javax.enterprise.context.RequestScoped;
-import javax.enterprise.inject.spi.BeanManager;
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @RequestScoped
 @Path("/")
 public class MyResource {
-  @Inject
-  BeanManager manager;
-
   @Path("/")
   @GET
-  public String getManager() {
-    return manager.toString();
+  public String index() {
+    return "Hello world";
   }
 }
